5799e337d0ad19a4920110558540cbcfa6b4e2eb
Lee Machin <leemachin@users.noreply.github.com>
2015-01-19 23:25:31 +0000

    require.js? really?

--- jquery.scroll.js
+++ jquery.scroll.js
@@ -1,15 +1,6 @@
-var req = function($) {
-  $.fn.scroll = function() {
-    $('html, body')
-      .animate({'scrollTop': this.offset().top}, 'slow', 'swing');
-  }
-};
-
-// So you can use require.js
-(function(req) {
-  if (typeof define === 'function' && define.amd) {
-    return req(['jQuery'], scroll)
-  } else {
-    return req(jQuery)
-  }
-})(req)
+jQuery.fn.scroll = function() {
+  $('html, body')
+    .animate({'scrollTop': function () {
+      this.offset().top
+    }}, 'slow', 'swing');
+}
